9cd243aeb03142c24a377d7b08de6f2de363ee6a
Dominik Zemirek <dominikzemirek00@gmail.com>
2022-06-26 16:43:41 +0200

    Pierwszy commit

---
+++ first.letter.py
@@ -0,0 +1,8 @@
+string1 = "Tell me your password :"
+string2 = "The first letter you entered : "
+prompt = string2
+prompt = string1
+user_input = input(prompt)
+print("The first letter you entered was: " + user_input.upper()[0])
+
+
